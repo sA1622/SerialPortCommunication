bdb5638cdf88fbb84781c88dd5975a230ea71d0a
sA1622 <101582943+sA1622@users.noreply.github.com>
2023-05-18 00:10:16 +0300

    Add files via upload

---
+++ Serial Recive Control Panel/Serial Recive Control Panel/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Serial_Recive_Control_Panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(543, 405);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(139, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 122);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Open Port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 122);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Close Port";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(139, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baud Rate";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(219, 122);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Close Software";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 470);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Serial Recive Control Panel/Serial Recive Control Panel/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO;
+
+namespace Serial_Recive_Control_Panel
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+            // System.Windows.Forms.Timer always runs on GUI thread 
+        }
+        void getAvailablePorts()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(ports);
+        }//gets the available ports
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text == "" || comboBox2.Text == "") MessageBox.Show("Please select a port and/or baud rate", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                else
+                {
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                    serialPort1.Open();
+                    button1.Enabled = false;
+                    button2.Enabled= true;
+                    backgroundWorker1.RunWorkerAsync(2000);
+                }
+            }
+            catch(UnauthorizedAccessException) {
+                MessageBox.Show("Unauthorized Access => COM port busy", "Denied", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy) backgroundWorker1.CancelAsync(); // WorkerSupportsCancellation = true (required to calcelasync)
+            textBox1.Text = "";
+            serialPort1.Close();
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            bool datainput = false;
+            string data = string.Empty; // the data string
+            while (!datainput)
+            {
+                data = serialPort1.ReadLine(); //reads line from the serial port
+                if (!string.IsNullOrEmpty(data)) datainput = true; // if data is null then datainput = true => it will 
+            }
+            if (datainput)
+            {
+                textBox1.Invoke((MethodInvoker)(() => textBox1.Text += "Data is:" + data + Environment.NewLine));//Lambda expression
+                // we invoke the function from another thread https://www.youtube.com/watch?v=rvMIIuRXmU4
+                try
+                {
+                    StreamWriter writer = new StreamWriter(Application.StartupPath,);
+                    //write to txt to update xml file 
+                }
+                catch(Exception)
+                {
+                    //blank
+                }
+            }
+            textBox1.Invoke((MethodInvoker)(() => textBox1.SelectionStart = textBox1.TextLength));
+            textBox1.Invoke((MethodInvoker)(() => textBox1.ScrollToCaret())); // autoscrolls the text in the textbox
+            /*try
+            {
+                textBox1.Text = serialPort1.ReadLine();
+
+            }
+            catch(TimeoutException) {
+                MessageBox.Show("Timeout", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }*/ // displays one line at a time
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           e.Cancel = true; // prevents Software from closing by pressing the "X" button
+            MessageBox.Show("Close the Software from the close button", "Exit", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                if (!backgroundWorker1.IsBusy) backgroundWorker1.CancelAsync(); // WorkerSupportsCancellation = true (required to cancelasync)
+                serialPort1.Close();
+                button1.Enabled = true;
+                button2.Enabled = false;
+                textBox1.Text = "";
+                MessageBox.Show("SERIAL PORT CLOSED", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information); //if program is being closed and if the serialPort is still opened => close the serial port and notify the user with a MessageBox
+            }
+            System.Windows.Forms.Application.ExitThread(); // exits Software
+        }
+    }
+}
